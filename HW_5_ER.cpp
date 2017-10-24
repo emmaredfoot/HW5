8fcb5209d82677c7388fd1a163ee40e508e26da4
Emma <eredfoot@gmail.com>
2017-10-24 11:40:31 -0600

    First Commit

---
+++ HW_5_ER.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : HW_5_ER.cpp
+// Author      : Emma Redfoot
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Pointers
+//============================================================================
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+Point parray[4];
